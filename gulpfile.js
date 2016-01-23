5c104e2a74bd181b1ead7ca3855016378a1e90f4
Anton Zabelin <libarus@gmail.com>
2016-01-24 00:16:01 +0300

    Промежуточный вариант

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,18 @@
-var gulp = require("gulp"),
-    concatCSS = require('gulp-concat-css'),
-    rename = require('gulp-rename'),
-    prefix = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css'),
-    sass = require('gulp-sass');
+var gulp = require("gulp")
+    ,concatCSS = require('gulp-concat-css')
+    ,rename = require('gulp-rename')
+    ,prefix = require('gulp-autoprefixer')
+    ,minifyCSS = require('gulp-minify-css')
+    // ,sass = require('gulp-sass')
+    ;
 
 gulp.task('default', function() {
-    gulp.src('./app_dev/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./app/css/'));
-
-    /**/
     gulp.src('./app_dev/css/*.css')
-        .pipe(concatCSS('bundle.css'))
-        .pipe(prefix('last 15 versions'))
+        //.pipe(concatCSS('bundle.css'))
+        .pipe(prefix('last 25 versions'))
         //.pipe(minifyCSS())
-        .pipe(rename('bundle.min.css'))
+        //.pipe(rename('bundle.min.css'))
         .pipe(gulp.dest('./app/css/'));
-    /**/
 });
 
 gulp.task('watch',function () {
